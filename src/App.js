a9b418a78fa9746491ca4ebc37536e32b12782df
David Engel <Davidjengel2012@gmail.com>
2019-06-24 23:08:19 -0600

    Get fetch call working, buildout App.js state

--- src/App.js
+++ src/App.js
@@ -4,24 +4,43 @@ import Form from './Form'
 import DarkSkyApi from 'dark-sky-api';
 import './CSS/index.scss';
 
-// DarkSkyApi.apiKey = '2970abeb55f410d97c916884401dcbd7';
+const API_KEY = 'c6e5fa0baf121d492ea445e4808ba14d'
 
 
 // const currentWeather;
 // const weekForecast;
 
 class App extends React.Component {
-
-  getWeather = async() => {
-    const api_call = await fetch('https://api.darksky.net/forecast/2970abeb55f410d97c916884401dcbd7/39.73915,-104.9847/?exclude=currently,minutely');
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value ;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
     const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.syts.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    })
   }
 
   render(){
     return (
       <div className="App">
         <Header />
-        <Form />
+        <Form getWeather={this.getWeather}/>
       </div>
     );
   }  
